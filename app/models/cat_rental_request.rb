782c8f2f66d4d6d76b7f33426022efb6fb6d841b
Hank Fanchiu <hank@fanchiu.com>
2015-11-17 20:16:09 -0800

    Fix cat_rental_request#approve! transaction to deny overlapping requests

--- app/models/cat_rental_request.rb
+++ app/models/cat_rental_request.rb
@@ -2,16 +2,16 @@ class CatRentalRequest < ActiveRecord::Base
   STATUSES = %w(PENDING APPROVED DENIED)
   validates :cat_id, :start_date, :end_date, presence: true
   validates :status, inclusion: { in: STATUSES }
-  validate :new_request_does_not_overlap
+  validate :request_does_not_overlap_with_approved
 
   belongs_to :cat
 
   def approve!
     transaction do
-      self.update(status: 'APPROVED')
       overlapping_pending_requests.each do |overlap|
         overlap.update(status: 'DENIED')
       end
+      self.update(status: 'APPROVED')
     end
   end
 
@@ -19,7 +19,6 @@ class CatRentalRequest < ActiveRecord::Base
     self.update(status: 'DENIED')
   end
 
-  private
   def overlapping_requests
     dates = { start_date: self.start_date, end_date: self.end_date }
     same_cat = self.class.where(cat_id: self.cat_id)
@@ -27,6 +26,7 @@ class CatRentalRequest < ActiveRecord::Base
       (start_date BETWEEN :start_date AND :end_date)
       OR (end_date BETWEEN :start_date AND :end_date)
     SQL
+    overlap.where.not(id: self.id)
   end
 
   def overlapping_pending_requests
@@ -37,9 +37,9 @@ class CatRentalRequest < ActiveRecord::Base
     overlapping_requests.where(status: 'APPROVED')
   end
 
-  def new_request_does_not_overlap
+  def request_does_not_overlap_with_approved
     unless overlapping_approved_requests.empty?
-      errors[:new_request] << "overlaps with an approved request."
+      errors[:request] << "has time overlap with an approved request."
     end
   end
 end
