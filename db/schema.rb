d9c82ce9a671b09b8aff42c5b43f5fe77939a302
Hank Fanchiu <hank@fanchiu.com>
2015-11-18 21:17:16 -0800

    Add User model and table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151117233257) do
+ActiveRecord::Schema.define(version: 20151119051335) do
 
   create_table "cat_rental_requests", force: :cascade do |t|
     t.integer  "cat_id",                         null: false
@@ -33,5 +33,16 @@ ActiveRecord::Schema.define(version: 20151117233257) do
     t.datetime "created_at",            null: false
     t.datetime "updated_at",            null: false
   end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "username",        null: false
+    t.string   "password_digest", null: false
+    t.string   "session_token",   null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+  end
+
+  add_index "users", ["session_token"], name: "index_users_on_session_token", unique: true
+  add_index "users", ["username"], name: "index_users_on_username", unique: true
 
 end
