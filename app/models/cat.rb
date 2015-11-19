f9bd79405e1fd6de6d5f5da10818aa1866bd4fe3
Hank Fanchiu <hank@fanchiu.com>
2015-11-18 20:49:22 -0800

    Refactor Cat model, cat's _form and show.html.erb

--- app/models/cat.rb
+++ app/models/cat.rb
@@ -12,36 +12,19 @@ class Cat < ActiveRecord::Base
   SEXES = { "M" => "Male", "F" => "Female" }
 
   validates :birth_date, :color, :name, :sex, :description, presence: true
-  validates :sex, inclusion: { in: SEXES.keys }
-  validates :color, inclusion: { in: COLORS.keys }
+  validates :sex, inclusion: SEXES.keys
+  validates :color, inclusion: COLORS.keys
   validate :birth_date_not_in_future
 
   has_many :cat_rental_requests, dependent: :destroy
 
   def age
     now = Date.today
-    years = now.year - Date.parse(birth_date).year
-    years
+    now.year - Date.parse(birth_date).year
   end
 
   def sorted_requests
     cat_rental_requests.order(:start_date)
-  end
-
-  def display_sex
-    SEXES[sex]
-  end
-
-  def display_color
-    COLORS[color]
-  end
-
-  def all_colors
-    COLORS
-  end
-
-  def all_sexes
-    SEXES
   end
 
   private
